1cc181f641c68bec4bede5d11f23618d4ff66628
fahad <fahad@binate-solutions.com>
2026-01-28 20:35:49 +0600

    gg

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qw@nazzf+xln7jsf)8x4l1ddeeg_amwz*8!-9!)234irj)zy1b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
